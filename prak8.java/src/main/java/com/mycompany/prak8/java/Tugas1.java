67a4fddae2b96c471800d328f332f9bed140bfff
Musfarazn <112687844+Musfarazn@users.noreply.github.com>
2022-10-20 21:03:20 +0700

    Add files via upload

---
+++ prak8.java/src/main/java/com/mycompany/prak8/java/Tugas1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prak8.java;
+
+/**
+ *
+ * @author Musfara Zahra Nadien
+ */
+public class Tugas1 {
+    public static void main(String[] args) {
+        
+        String[] days = {"Monday" , "Tuesday" , "Wednesday" , "Thursday" , "Friday" , "Saturday" , "Sunday"};
+
+        for(int i = 0; i < days.length; i++)
+        {
+            System.out.print(days[i]);
+            System.out.print(" ");
+        }
+    }
+}
